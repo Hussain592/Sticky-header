10f9ba05707d971e3c53f80fb6afcb63b0e0fe64
Hussain <perryscott1516@gmail.com>
2024-10-04 03:16:13 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+const header = document.querySelector("#header");
+
+const sticky = header.offsetTop;
+console.log(sticky);
+
+
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset > sticky) {
+      header.classList.add("sticky");
+    } else {
+      header.classList.remove("sticky");
+      console.log(sticky);
+      
+    }
+});
+
+
+
